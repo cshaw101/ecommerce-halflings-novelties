2b54807dee727b1d68cf0e5f28633faa0f047ac7
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-07 12:47:12 -0400

    nav tuned

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,30 +4,23 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Link } from 'react-router-dom';
+
 
 export default function ButtonAppBar() {
     return (
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
           <Toolbar>
-          <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link to="/" style={{ textDecoration: 'none', color: 'inherit', flexGrow: 1 }}>
+            <Typography variant="h4" component="div">
               Halflings Novelties
             </Typography>
-            <Button color="inherit">Shop</Button>
-            <Button color="inherit">About</Button>
-            <Button color="inherit"><ShoppingCartIcon/></Button>
+          </Link>
+            <Button href='/store' color="inherit">Shop</Button>
+            <Button href='/about' color="inherit">About</Button>
+            <Button href='shoppingcart' color="inherit"><ShoppingCartIcon/></Button>
           </Toolbar>
         </AppBar>
       </Box>
