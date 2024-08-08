1d122c6a19319004bf581cec93b6238559c167fb
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-08 16:59:25 -0400

    route is working

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-const app = express();
+app.get('/test', (req, res) => {
+    res.send('Server is working!');
+  });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
