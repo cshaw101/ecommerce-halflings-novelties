7819b733d8e3a4918bf4fc7a0a950ddbf83f3528
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-09 13:55:51 -0400

    stripe test is working

--- server/index.js
+++ server/index.js
@@ -1,11 +1,38 @@
-const express = require("express");
+const stripe = require('stripe')('sk_test_51PlDV7BiccO5TC0vkqb9gggPYzZkoCBsCy40Kjc8z7akzugBw1CRMt1Yq5BAVxpX7yOth6rTHyUpYZs4Ei8qv4xj00gU74FsXo');
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const YOUR_DOMAIN = 'http://localhost:3000';
+
+app.use(express.static('public'));
+app.use(express.json());
+
 app.get('/test', (req, res) => {
-    res.send('Server is working!');
-  });
+  res.send('Server is working!');
+});
+
+app.post('/create-checkout-session', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: 'price_1PlbyfBiccO5TC0vJ5jlOkPc',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${YOUR_DOMAIN}/?success=true`,
+      cancel_url: `${YOUR_DOMAIN}/?canceled=true`,
+    });
+
+    res.redirect(303, session.url);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import BasicButtons from '../styles/BasicButton';
+import React, { useState, useEffect } from 'react';
+//import BasicButtons from '../styles/BasicButton';
+import Message from './Message';
+import ProductDisplay from './ProductDisplay';
 
 
 const ShoppingCart = () => {
-  return (
-    <div>
-      <h1>ShoppingCart</h1>
-      <BasicButtons to='/checkout' label="Checkout" variant="contained" />
-    </div>
-  )
-}
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
 
+    if (query.get("success")) {
+      setMessage("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      setMessage(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+    }
+  }, []);
+
+  return message ? (
+    <Message message={message} />
+  ) : (
+    <ProductDisplay />
+  );
+}
 export default ShoppingCart
