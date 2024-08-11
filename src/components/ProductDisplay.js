e12eb47849d87a6fee6a7ec87ee09bce511b31a8
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-11 11:25:22 -0400

    added style to price card

--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -1,25 +1,39 @@
-import React from 'react'
+import * as React from 'react'
 import BasicButtons from '../styles/BasicButtons'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+//import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-const ProductDisplay = () => (
-    <section>
-      <div className="product">
-        <img
-          src="https://i.imgur.com/EHyR2nP.png"
-          alt="The cover of Stubborn Attachments"
-        />
-        <div className="description">
-        <h3>Page Deoderant</h3>
-        <h5>$100.00</h5>
-        </div>
-      </div>
+export default function ProductDisplay() {
+return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="The cover of Stubborn Attachments"
+        height="140"
+        image="https://i.imgur.com/EHyR2nP.png"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        Page Deoderant
+        </Typography>
+        <Typography variant="body1" color="text.primary">
+        This is the worlds strongest Deoderant meant for only the toughest jobs
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+        $100.00
+        </Typography>
+      </CardContent>
+      <CardActions>
       <form action="http://localhost:3001/create-checkout-session" method="POST">
       <BasicButtons label="Checkout" variant="contained" type="submit" />
-        
       </form>
-    </section>
+      </CardActions>
+    </Card>
   );
-
- export default ProductDisplay
+};
 
 
