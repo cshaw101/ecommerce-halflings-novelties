97af13e40bb170a76943268d652963b616fd2126
Caleb Shaw <shawcaleb1688@gmail.com>
2024-08-12 11:41:17 -0400

    added skeleton for loading products

--- src/components/Store.js
+++ src/components/Store.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Grid from '@mui/material/Grid';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
+import Skeleton from '@mui/material/Skeleton';
 import useProducts from '../mockProducts';
 import ProductDisplay from './ProductDisplay';
 
@@ -19,25 +20,36 @@ const Store = () => {
     page * productsPerPage
   );
 
-  if (loading) return <p>Loading...</p>;
+  const skeletonArray = Array.from(new Array(productsPerPage));
+
   if (error) return <p>Error loading products: {error.message}</p>;
 
   return (
     <div>
       <h1>Store</h1>
       <Grid container spacing={2}>
-        {displayedProducts.map((product) => (
-          <Grid item xs={12} sm={6} md={4} key={product.id}>
-            <ProductDisplay
-              name={product.title}
-              description={product.description}
-              price={`$${product.price}`}
-              imageUrl={product.thumbnail}
-              stock={product.stock}
-              product={product}  // Pass the entire product object
-            />
-          </Grid>
-        ))}
+        {loading
+          ? skeletonArray.map((_, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <Skeleton variant="rectangular" width="100%" height={200} />
+                <Skeleton width="60%" />
+                <Skeleton width="40%" />
+                <Skeleton width="100%" />
+                <Skeleton width="80%" />
+              </Grid>
+            ))
+          : displayedProducts.map((product) => (
+              <Grid item xs={12} sm={6} md={4} key={product.id}>
+                <ProductDisplay
+                  name={product.title}
+                  description={product.description}
+                  price={`$${product.price}`}
+                  imageUrl={product.thumbnail}
+                  stock={product.stock}
+                  product={product} // Pass the entire product object
+                />
+              </Grid>
+            ))}
       </Grid>
       <Stack spacing={2} sx={{ marginTop: '20px', alignItems: 'center' }}>
         <Pagination
